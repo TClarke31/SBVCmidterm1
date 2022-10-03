fa19057dafb953f021003cf902a4509259869bdd
TClarke31 <112362108+TClarke31@users.noreply.github.com>
2022-10-02 21:52:18 -0700

    Initial commit

---
+++ app/src/main/java/com/example/sbvcloginapp/loginsbvc.java
@@ -0,0 +1,45 @@
+package com.example.sbvcloginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class loginsbvc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginsbvc);
+        
+        
+        TextView username=(TextView)  findViewById(R.id.username);
+        TextView password=(TextView)  findViewById(R.id.password);
+        
+        MaterialButton loginbtn= (MaterialButton) findViewById(R.id.loginbtn);
+
+
+         
+         
+        //admin and admin 
+        
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context;
+                if(username.getText().toString().equals("admin") && password.getText().toString(). equals("admin")){
+                    //correct
+                    Toast.makeText(context:loginsbvc.this, text:"Login Successfull",Toast.LENGTH_SHORT).show();
+                }else
+                    //incorrect
+                   Toast.makeText(context:loginsbvc.this,text: "Login Failed",Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+    }
+}
